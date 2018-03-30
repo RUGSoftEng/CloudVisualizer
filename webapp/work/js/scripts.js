6ee33eca269f8abd8fe141e9c38e70a3b49a25ac
biancaudrescu <biancaudrescu@yahoo.com>
2018-03-30 23:41:28 +0200

    added a slider prototype for the virtual machine

--- webapp/work/js/scripts.js
+++ webapp/work/js/scripts.js
@@ -1,4 +1,4 @@
-
+//drag
   $(document).ready(function(){
        $("#myAccordion").accordion();
        $(".source li").draggable({helper:"clone"});
@@ -6,10 +6,16 @@
            $("#items").append($("<li></li>").text(ui.draggable.text()).on("click",function() { $(this).remove()}));
        }});
   });
-  
+
   function clearBox(elementID){
     document.getElementById(elementID).innerHTML = "";
   }
 
-  
-  
+  //sliders
+var slider = document.getElementById("myRange");
+var output = document.getElementById("value");
+output.innerHTML = slider.value;
+
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}
