cd4af9ca741ca4533fddb86d114f7cb708ae2035
biancaudrescu <biancaudrescu@yahoo.com>
2018-04-22 11:19:16 +0200

    changed back to draggable text and added modal for general provider filter

--- webapp/work/js/scripts.js
+++ webapp/work/js/scripts.js
@@ -1,9 +1,9 @@
 //drag
-  $(document).ready(function(){
+$(document).ready(function(){
        $("#myAccordion").accordion();
-       $(".source img").draggable({helper:"clone"});
+       $(".source li").draggable({helper:"clone"});
        $("#canvas").droppable({drop:function(event,ui){
-       $("#items").append($("<li></li>").img(ui.draggable.img()));
+           $("#items").append($("<li></li>").text(ui.draggable.text()).on("click",function() { $(this).remove()}));
        }});
   });
 
@@ -49,4 +49,30 @@
   slider5.oninput = function() {
     output5.innerHTML = this.value;
   }
+
+  // Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("provider");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
   });
