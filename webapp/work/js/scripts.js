d7a30b2bef950669a0a9ab3fd77c683c6614f094
biancaudrescu <biancaudrescu@yahoo.com>
2018-04-30 15:12:12 +0200

    added list to modal , styled etc

--- webapp/work/js/scripts.js
+++ webapp/work/js/scripts.js
@@ -55,6 +55,7 @@ var modal = document.getElementById('myModal');
 
 // Get the button that opens the modal
 var btn = document.getElementById("provider");
+var btn1 = document.getElementById("save-modal");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
@@ -63,7 +64,10 @@ var span = document.getElementsByClassName("close")[0];
 btn.onclick = function() {
     modal.style.display = "block";
 }
-
+//When the user clicks on Save, close the modal
+btn1.onclick=function() {
+    modal.style.display = "none";
+}
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
     modal.style.display = "none";
