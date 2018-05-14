759bf84dcc11305f834fdfacf7dc69bab56bf6c8
HolmesXVIII <klaastilman@hotmail.com>
2018-05-14 18:37:41 +0200

    Merge with frontend

--- MVP Prototype/webapp/work/js/main.js
+++ MVP Prototype/webapp/work/js/main.js
@@ -6,6 +6,15 @@ var storageSize;
 var DBSize;
 var calculate;
 
+//drag
+$(document).ready(function(){
+    $("#myAccordion").accordion();
+    $(".source li").draggable({helper:"clone"});
+    $("#canvas").droppable({drop:function(event,ui){
+        $("#items").append($("<li></li>").text(ui.draggable.text()).on("click",function() { $(this).remove()}));
+    }});
+});
+
 // from canvasObject
 var VirtualMachines=[];
 var Databases=[];
@@ -163,6 +172,7 @@ function dragVM(ev) {
 }
 
 function drop(ev) {
+    console.log("jim");
     ev.preventDefault();
     var obj = JSON.parse(ev.dataTransfer.getData("foo"));
     if (obj.objectName === "VirtualMachine") {
@@ -279,4 +289,61 @@ $(function() {
     DBSlider.oninput = function() {
         DBSize.innerHTML = this.value;
     }
+
+    // Get the modal
+    var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+    var btn = document.getElementById("provider");
+    var btn1 = document.getElementById("save-modal");
+
+// Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+    btn.onclick = function() {
+        modal.style.display = "block";
+    }
+//When the user clicks on Save, close the modal
+    btn1.onclick=function() {
+        modal.style.display = "none";
+    }
+// When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+// When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+
+    }
 });
+
+//show div when calculate is clicked
+function Calculate() {
+    var x = document.getElementById("canvas-pop-up");
+    if (x.style.display = "none") {
+        x.style.display = "block";
+    }
+}
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("top").style.display = "block";
+    } else {
+        document.getElementById("top").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+
